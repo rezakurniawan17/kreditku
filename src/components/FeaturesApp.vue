deda135d2a165163b9470ec900c77f19bbfd21b2
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2021-04-05 22:20:06 +0700

    dist

--- src/components/FeaturesApp.vue
+++ src/components/FeaturesApp.vue
@@ -2,17 +2,17 @@
   <section id="features-app">
     <div class="container lg:relative px-5 lg:px-0 mt-12 lg:mt-0 flex lg:flex-row flex-col w-full lg:w-10/12 2xl:w-8/12 xl:w-8/12 lg:p-3 xl:p-4 p-5 mx-auto rounded-lg lg:shadow-primary lg:top-[-60px] xl:top-[-90px] bg-white">
       <div class="p-5 text-center lg:p-4 lg:w-1/3">
-        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/prime-time 1.svg" alt="">
+        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/prime-time.svg" alt="">
         <h5 class="mt-3 text-lg font-bold md:text-xl xl:text-2xl text-title">Proses Cepat</h5>
         <p class="mt-2 text-captions lg:w-full md:w-8/12 md:mx-auto lg:text-sm xl:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus</p>
       </div>
       <div class="p-5 text-center lg:p-4 lg:w-1/3">
-        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/target 1.svg" alt="">
+        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/target.svg" alt="">
         <h5 class="mt-3 text-lg font-bold md:text-xl xl:text-2xl text-title">Tujuan Tepat</h5>
         <p class="mt-2 text-captions lg:w-full md:w-8/12 md:mx-auto lg:text-sm xl:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus</p>
       </div>
       <div class="p-5 text-center lg:p-4 lg:w-1/3">
-        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/circular-arrows 1.svg" alt="">
+        <img class="mx-auto md:w-28 lg:w-16" src="../assets/img/circular.svg" alt="">
         <h5 class="mt-3 text-lg font-bold md:text-xl xl:text-2xl text-title">Fleksibel</h5>
         <p class="mt-2 text-captions lg:w-full md:w-8/12 md:mx-auto lg:text-sm xl:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus</p>
       </div>
--- src/components/ProductApp.vue
+++ src/components/ProductApp.vue
@@ -9,22 +9,22 @@
           <h5 class="mb-4 text-3xl font-bold text-center lg:text-3xl lg:text-left text-heading">Produk Pembiayaan Kreditku</h5>
           <div class="grid grid-cols-1 gap-8 lg:gap-6 xl:gap-8 md:grid-cols-2">
             <div class="w-11/12 h-auto px-6 py-8 mx-auto text-center lg:w-full lg:px-4 xl:px-6 lg:py-3 rounded-xl shadow-primary">
-              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/truck 1.svg" alt="">
+              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/truck.svg" alt="">
               <h6 class="mt-4 text-xl font-bold text-title">Truck</h6>
               <p class="mt-2 text-captions">Fasilitas pembiayaan mobil Truck jenis Light Truck dan Heavy Truck</p>
             </div>
             <div class="w-11/12 h-auto px-6 py-8 mx-auto text-center lg:w-full lg:px-4 xl:px-6 lg:py-3 shadow-primary rounded-xl">
-              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/delivery-van 2.svg" alt="">
+              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/delivery-van.svg" alt="">
               <h6 class="mt-4 text-xl font-bold text-title">Angkutan</h6>
               <p class="mt-2 text-captions">Fasilitas pembiayaan mobil Angkutans jenis Minibus atau Mikrobus</p>
             </div>
              <div class="w-11/12 h-auto px-6 py-8 mx-auto text-center lg:w-full lg:px-4 xl:px-6 lg:py-3 shadow-primary rounded-xl">
-              <img class="items-center inline-block xl:w-24lg:w-16" src="../assets/img/delivery-truck 3.svg" alt="">
+              <img class="items-center inline-block xl:w-24lg:w-16" src="../assets/img/delivery-truck.svg" alt="">
               <h6 class="mt-4 text-xl font-bold text-title">Pick Up</h6>
               <p class="mt-2 text-captions">Fasilitas pembiayaan mobil Pick Up model Box maupun model Bak</p>
             </div>
             <div class="w-11/12 h-auto px-6 py-8 mx-auto text-center lg:w-full lg:px-4 xl:px-6 lg:py-3 shadow-primary rounded-xl">
-              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/jeep 1.svg" alt="">
+              <img class="items-center inline-block xl:w-24 lg:w-16" src="../assets/img/jeep.svg" alt="">
               <h6 class="mt-4 text-xl font-bold text-title">Pessenger</h6>
               <p class="mt-2 text-captions">Fasilitas pembiayaan mobil Sedan, MPV dan SUV</p>
             </div>
